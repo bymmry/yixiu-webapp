1c778d54ae0fd1061e4b3d11eab22f1a39225fc6
yupeng0813 <1072172744@qq.com>
2018-03-08 12:55:30 +0800

    npm run build

---
+++ dist/src/views/orders/api.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _promise = require('babel-runtime/core-js/promise');
+
+var _promise2 = _interopRequireDefault(_promise);
+
+exports.getOrderList = getOrderList;
+
+var _ajax = require('../../lib/ajax');
+
+var _ajax2 = _interopRequireDefault(_ajax);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ajax = _ajax2.default.ajax;
+var url = 'https://m.yixiutech.com';
+// const url = "http://yixiu.natappvip.cc";
+
+//根据筛选条件获取订单列表
+function getOrderList(req) {
+  console.log(req);
+  var order = {
+    // shop:'', //店铺id
+    user: req.user._id, //用户id
+    state: req.state //订单状态
+  };
+  return new _promise2.default(function (resolve, reject) {
+    ajax.post(url + '/order/service/filter', order).then(function (res) {
+      resolve(res);
+    }).then(function (err) {
+      reject(err);
+    });
+  });
+}
+//# sourceMappingURL=api.js.map
