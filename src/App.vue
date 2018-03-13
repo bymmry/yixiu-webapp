54e226dfe96f80579a026c6acb007ff005cf289e
Yupeng0813 <1072172744@qq.com>
2018-03-14 00:30:53 +0800

    V2.8.2

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
       // let userData = sessionStorage.getItem("userData");
       // userData = JSON.parse(userData);
       console.log(userData);
-      alert("openid:" + userData.openid);
+      // alert("openid:" + userData.openid);
       // alert("App paySuccess：" + userData.paySuccess);
       let pushData = this.reguserinfo(userData);
       // console.log(pushData)
