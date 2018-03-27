bcf3bb5c88fff5c834cceee3a49680910af41c55
Yupeng0813 <1072172744@qq.com>
2018-03-27 23:57:52 +0800

    add

--- src/views/sellerHome/pages/shopWallet.vue
+++ src/views/sellerHome/pages/shopWallet.vue
@@ -12,9 +12,17 @@
             </div>
         <!-- </van-pull-refresh> -->
         <div class="funcbtn">
-            <button>提现</button>
-            <button>退保证金</button>
+            <button @click="getMoney">提现</button>
+            <button @click="returnMoney" class="other">退保证金</button>
         </div>
+        <cube-popup :center="false" type="my-popup" ref="getMyMoney">
+            <div class="moneyBox">
+                <h3>请输入提现金额(元)</h3>
+                <input placeholder="0" v-model="MoneyValue">
+                <cube-button @click="sureGet">提现到微信</cube-button>
+                <cube-button class="bottom" @click="closeMoney">取消</cube-button>
+            </div>
+        </cube-popup>
     </div>
 </template>
 
@@ -23,7 +31,8 @@ export default {
     name: "shop-wallet",
     data(){
         return {
-            wallentMoney: 0
+            wallentMoney: 0,
+            MoneyValue: ""
         }
     },
     async created() {
@@ -43,6 +52,70 @@ export default {
             this.$router.push("/sellerHome");
         },
         getMoney: function(){
+            const getMyMoney = this.$refs.getMyMoney
+            getMyMoney.show();
+        },
+        sureGet: function(){
+            const getMyMoney = this.$refs.getMyMoney
+            let that = this;
+            if (this.MoneyValue != '') {
+                console.log(this.MoneyValue,this.wallentMoney)
+                if(Number(this.MoneyValue) >= Number(this.wallentMoney)){
+                    this.$createToast({
+                    type: 'warn',
+                    time: 1000,
+                    txt: '你没有那么多钱'
+                }).show()
+                }else{
+                    this.$createDialog({
+                        type: 'confirm',
+                        title: '提现到微信',
+                        content: that.MoneyValue + '元',
+                        confirmBtn: {
+                            text: '确定',
+                            active: true,
+                            disabled: false,
+                            href: 'javascript:;'
+                        },
+                        cancelBtn: {
+                            text: '取消',
+                            active: false,
+                            disabled: false,
+                            href: 'javascript:;'
+                        },
+                        onConfirm: () => {
+                            this.$createToast({
+                                type: 'warn',
+                                time: 1000,
+                                txt: '点击确认按钮'
+                            }).show()
+                        },
+                        onCancel: () => {
+                            getMyMoney.hide();
+                        }
+                    }).show()
+                }
+                
+            }else{
+                this.$createToast({
+                    type: 'warn',
+                    time: 1000,
+                    txt: '未输入金额'
+                }).show()
+            }
+
+            
+
+            // getMyMoney.hide();
+        },
+        returnMoney: function(){
+            alert("请联系客服");
+        },
+        closeMoney: function(){
+            const getMyMoney = this.$refs.getMyMoney
+            getMyMoney.hide();
+        },
+        moneyBackSure: function(){
 
         }
     }
@@ -89,11 +162,13 @@ export default {
     }
     .shopWallet .funcbtn{
         position: absolute;
+        display: flex;
         bottom: 0;
         left: 0;
         right: 0;
     }
     .shopWallet .funcbtn button{
+        flex: 1;
         border: none;
         background-color: #2796CB;
         float: left;
@@ -105,6 +180,41 @@ export default {
         font-size: 6vw;
         
         border-top: 1px solid #eee;
+    }
+    .shopWallet .funcbtn button.other{
+        background: #fff;
+        color: #2796CB;
+    }
+
+    .moneyBox{
+        width: 100vw;
+        height: 85vh;
+        margin: 0 auto;
+        background: #fff;
+    }
+    .moneyBox h3{
+        padding: 10px;
+        text-align: center;
+        height: 30px;
+        line-height: 30px;
+    }
+    .moneyBox input{
+        display: block;
+        margin: 0 auto;
+        padding: 5px 10px;
+        width: auto;
+        height: 30px;
+        border: 2px solid #eee;
+        border-radius: 5px;
+    }
+    .moneyBox button{
+        margin: 20px auto;
+        width: 60%;
+    }
+    .moneyBox button.bottom{
+        width: 100%;
+        position: absolute;
+        bottom: 0;
     }
 
 </style>
