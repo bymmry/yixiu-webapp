ecf6780a18bec827c6b826201f559507ba2370a9
Yupeng0813 <1072172744@qq.com>
2018-03-28 13:37:20 +0800

    a

--- src/views/sellerHome/App.vue
+++ src/views/sellerHome/App.vue
@@ -76,22 +76,22 @@
 			}
 			this.shopData = res.data;
 			console.log(res.data);
-			if(res.data.qualification){ //已缴纳保证金
-				this.content = [
-					{ name: '添加手机维修服务', icon: 'fuwu', link: '/service' },
-					{ name: '查看手机服务列表', icon: 'view', link: '/viewServices' },
-					{ name: '修改手机维修服务', icon: 'update', link: '/updateService' },
-					{ name: '二手手机交易', icon: 'publish', link: '/publishPhone' },
-					{ name: '删除已发布二手手机', icon: 'phone', link: '/deletePhone' },
-					{ name: '完善信息', icon: 'identification', link: '/updateMsg' },
-					{ name: '商家钱包', icon: 'wallet', link: '/shopWallet' }
-				];
-			}else { // 未缴纳保证金
-				console.log("未缴纳保证金");
-				this.content = [
-					{ name: '缴纳保证金', icon: 'baozhengjin', link: '/payBail' }
-				]
-			}
+			// if(res.data.qualification){ //已缴纳保证金
+			// 	this.content = [
+			// 		{ name: '添加手机维修服务', icon: 'fuwu', link: '/service' },
+			// 		{ name: '查看手机服务列表', icon: 'view', link: '/viewServices' },
+			// 		{ name: '修改手机维修服务', icon: 'update', link: '/updateService' },
+			// 		{ name: '二手手机交易', icon: 'publish', link: '/publishPhone' },
+			// 		{ name: '删除已发布二手手机', icon: 'phone', link: '/deletePhone' },
+			// 		{ name: '完善信息', icon: 'identification', link: '/updateMsg' },
+			// 		{ name: '商家钱包', icon: 'wallet', link: '/shopWallet' }
+			// 	];
+			// }else { // 未缴纳保证金
+			// 	console.log("未缴纳保证金");
+			// 	this.content = [
+			// 		{ name: '缴纳保证金', icon: 'baozhengjin', link: '/payBail' }
+			// 	]
+			// }
 
 			// let data = { shop: '5a9fe2a27c67ee2f8c98c9d5', state: 12 }
 			// let res = await this.$api.sendData('https://yixiu.natappvip.cc/order/service/filter', data);
