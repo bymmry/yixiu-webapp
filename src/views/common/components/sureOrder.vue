4c516836400eb0918a74f54c4fcfffee71e63abe
Yupeng0813 <1072172744@qq.com>
2018-04-15 11:46:46 +0800

    支付测试

--- src/views/common/components/sureOrder.vue
+++ src/views/common/components/sureOrder.vue
@@ -137,9 +137,10 @@
                 WeixinJSBridge.invoke(
                     'getBrandWCPayRequest', sign.data,
                     function(wxres){     
-                      alert(JSON.stringify(wxres));
+                      alert(JSON.stringify(res));
+                      alert(JSON.stringify(payInfo));
                         if(wxres.err_msg == "get_brand_wcpay_request:ok" ) {
-                          that.paySuccess(res._id);
+                          that.paySuccess(res.id);
                         }else{
                           that.$toast("支付失败");
                         }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
