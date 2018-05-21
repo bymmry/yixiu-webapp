b512380f1dc65674c9b85432ff3ee4fb637c146c
longdong18 <349613352@qq.com>
2018-05-21 22:12:26 +0800

    Signed-off-by: longdong18 <349613352@qq.com>

--- src/views/my/pages/loginWx/loginWx.vue
+++ src/views/my/pages/loginWx/loginWx.vue
@@ -1,220 +1,68 @@
-<template>
-  <div class="myinfo-container">
-    <van-nav-bar
-      title="分享"
-      left-text="返回"
-      fixed
-      left-arrow
-      @click-left="prepage"
-    />
-    <!-- 顶部留白 -->
-    <div class="topblank"></div>
-    <button @click="authLogout">登录注销</button>
-    <button @click="setStorage">存储数据</button>
-    <button @click="getStorage">显示数据</button>
-    <div>
-      <div class="funcbtn">
-        <button @click="serchmoney" class="other">登录授权</button>
-        <button @click="getmoney">获取信息</button>
-      </div>
-    </div>
-  </div>
 
-</template>
-
-<script>
-  import { NavBar, Button, Dialog} from 'vant';
-  export default {
-    data () {
-      return {
-        infoName: '登录测试',
-        number: 0,
-        allnumber: 0,
-        surplusnumber: 0,
-        userlist: [],
-        allUserIds: [],
-        auths: null,
-        // userids: [],
-        // userOrderlists: [],
-      }
-    },
-    components: {
-      [NavBar.name]: NavBar,
-      [Button.name]: Button,
-      [Dialog.name]: Dialog,
-    },
-    methods: {
-      //导航栏 前往个人中心
-      prepage(){
-        this.$router.push({ path: "/my" })
-      },
-		
-		// Remove storage item
-      setStorage() {
-		plus.storage.setItem("longdong","18584664675");
-	  },
-      getStorage() {
-		alert(plus.storage.getItem("longdong"));
-	  },
-      serchwx () {
-		this.auths = null;
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+    <script type="text/javascript" charset="utf-8">
+      	mui.init();
+		var auths=null;
 		// 监听plusready事件  
 		document.addEventListener( "plusready", function(){
 			// 扩展API加载完毕，现在可以正常调用扩展API
 			plus.oauth.getServices( function(services){
-                alert(JSON.stringify(e));
-				this.auths = services;
+				console.log(JSON.stringify(services));
+				auths = services;
 			}, function(e){
 				alert( "获取分享服务列表失败："+e.message+" - "+e.code );
 			} );
 		}, false );
-      },
-      async serchmoney () {
-		let s = this.auths[0];
-		alert(JSON.stringify(s));
-		if ( !s.authResult ) {
-			s.login( function(e){
-				alert( "登录认证成功！" );
-			}, function(e){
-				alert( "登录认证失败！" );
-			} );
-		}else{
-			alert( "已经登录认证！" );
+		// 登录操作
+		function authLogin(){
+			alert(JSON.stringify(auths));
+			console.log(auths[0].id);
+			console.log(auths[0].description);
+			console.log(JSON.stringify(auths));
+			var s = auths[0];
+			if ( !s.authResult ) {
+				s.login( function(e){
+					alert( "登录认证成功！" );
+				}, function(e){
+					alert( "登录认证失败！" );
+				} );
+			}else{
+				alert( "已经登录认证！" );
+			}
 		}
-      },
-      async getmoney () {
-		let s = this.auths[0];
-		if ( !s.authResult ) {
-			alert("未登录授权！");
-		} else {
-			s.getUserInfo( function(e){
-				alert( "获取用户信息成功："+JSON.stringify(s.userInfo) );
-			}, function(e){
-				alert( "获取用户信息失败："+e.message+" - "+e.code );
-			} );
-		}
-      },
-     async authLogout () {
-        let auth = this.auths;
-        for (var i in auth) {
-            var s = auth[i];
-            if (s.authResult) {
-                s.logout(function(e) {
-                    console.log("注销登录认证成功！");
-                }, function(e) {
-                    console.log("注销登录认证失败！");
-                });
+		// 获取登录用户信息操作
+		function authUserInfo(){
+			var s = auths[0];
+			if ( !s.authResult ) {
+				alert("未登录授权！");
+			} else {
+				s.getUserInfo( function(e){
+					alert( "获取用户信息成功："+JSON.stringify(s.userInfo) );
+				}, function(e){
+					alert( "获取用户信息失败："+e.message+" - "+e.code );
+				} );
+			}
+		}	
+		//注销登录
+		function authLogout() {
+            for (var i in auths) {
+                var s = auths[i];
+                if (s.authResult) {
+                    s.logout(function(e) {
+                        console.log("注销登录认证成功！");
+                    }, function(e) {
+                        console.log("注销登录认证失败！");
+                    });
+                }
             }
         }
-     },
-    created() {
-        plusReady ();
-        document.addEventListener("plusready",plusReady,false);
-		this.auths = null;
-		// 监听plusready事件  
-		document.addEventListener( "plusready", function(){
-			// 扩展API加载完毕，现在可以正常调用扩展API
-			plus.oauth.getServices( function(services){
-				this.auths = services;
-			}, function(e){
-				alert( "获取分享服务列表失败："+e.message+" - "+e.code );
-			} );
-		}, false );
-    },
-    mounted () {
-        this.serchwx();
-    },
-  }
-}
-</script>
-
-<style scoped>
-  .myinfo-container{
-    min-height: 90vh;
-    padding: 2vh 3vw 5vh 3vw;
-    background: #fff;
-    
-  }
-  .topblank{
-    margin-top: 45.6px;
-  }
-  .messageTag{
-    border: 0.1px solid rgb(240, 241, 245);
-    padding: 3vh 4vw 4vh 4vw;
-    margin-bottom: 4vh;
-  }
-  .myinfo-title-container{
-    display: flex;
-    flex-direction: row;
-    justify-content:space-between;
-    align-items: center;
-    margin-bottom: 2vh;
-  }
-  .myinfo-title{
-    color: #727272;
-    font-size: 4vw;
-  }
-  .row-line{
-    width: 100%;
-    height: 0.3vh;
-    margin-top: 3vh;
-    margin-bottom: 3vh;
-    background: #ecebeb;
-  }
-  .row-line-money{
-    width: 100%;
-    height: 0.2vh;
-    margin-top: 2vh;
-    margin-bottom: 1vh;
-  }
-  .money{
-    margin-left: 5vh;
-    margin-bottom: 2vh;
-  }
-  .funcbtn button.other{
-        background: #fff;
-        color: #2796CB;
-  }
-  .funcbtn{
-      position: absolute;
-      display: flex;
-      z-index: 100;
-      bottom: 0;
-      left: 0;
-      right: 0;
-  }
-  .funcbtn button{
-      flex: 1;
-      border: none;
-      background-color: #4991e5;
-      float: left;
-      padding: 0;
-      width: 100vw;
-      line-height: 60px;
-      text-align: center;
-      color: #fff;
-      font-size: 6vw;
-        
-      border-top: 1px solid #eee;
-  }
-  .myinfo-message{
-    position: relative;
-    display: flex;
-    flex-direction: column;
-  }
-  .qrcodeBox{
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .tips{
-    font-size: 3.8vw;
-    margin-top: 3vh;
-    text-align: center;
-    color: #323643;
-  }
-  .tips span{
-    color: #4BC2C5;
-  }
-</style>
+    </script>
+</head>
+<body>
+	<span class="mui-icon weixin" id="weixin">服务列表</span> 
+	<button onclick="authLogin()">登录</button>
+	<button onclick="authUserInfo()">获取用户信息</button>
+	<button onclick="authLogout()">注销</button>
+</body>
