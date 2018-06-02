fdf48ef90c2f40a612075a452ca5d02436285ab4
Yupeng0813 <1072172744@qq.com>
2018-06-02 22:58:08 +0800

    pay h5 success

--- src/views/common/components/sureOrder.vue
+++ src/views/common/components/sureOrder.vue
@@ -197,7 +197,7 @@
               console.log(href);
               // window.location.href = `https://${href}`;
               alert(href);
-              window.open(href)
+              // window.open(href)
 
               console.log(this.$refs.surePays.$children[0])
               this.$refs.surePays.$children[0].show()
